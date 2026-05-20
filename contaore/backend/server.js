75e18bee895ca19b4c152050abc0c9e9ca467817
alexbonny1 <120141189+alexbonny1@users.noreply.github.com>
2026-05-20 14:59:31 +0200

    Update server.js

--- contaore/backend/server.js
+++ contaore/backend/server.js
@@ -34,7 +34,7 @@ await fastify.register(adminRoutes)
 fastify.get('/', async () => {
   return { status: 'ok' }
 })
-
+//test deploy
 fastify.get('/api/debug-tz', async (request, reply) => {
   const now = new Date()
   return {
